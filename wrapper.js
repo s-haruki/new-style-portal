305c9e5d9f20377ea759a808d0ec16cc0c84b325
Suekuni Haruki <130211284+s-haruki@users.noreply.github.com>
2023-04-16 01:25:33 +0900

    Update wrapper.js

--- wrapper.js
+++ wrapper.js
@@ -3,7 +3,7 @@
 // @namespace    wrapper_script_in_tempermonkey
 // @version      0.1
 // @description  wrapper in tempermonkey
-// @author       You
+// @author       s-haruki
 // @match        https://portal.onomichi-u.ac.jp/up/faces/up/*
 // @grant        none
 // @require      file:///C:/Users/suku_/Project/NewStyle/NewStyle.js
